ba9675c1eeb9923dc8bf85738b228597dadb1c80
Teraoka Akihiro <macrosfronter7@gmail.com>
2018-04-21 19:30:25 +0900

    Add files via upload

---
+++ Scripts/HandSpinner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandSpinner : MonoBehaviour
+{
+
+	private Transform _transform;
+	private Vector3 mousePosition;
+	private Vector3 screenPosition;
+  private Vector3[] prevPosition = new Vector3[10];  // nフレームの平均をとる
+  private int prevPositionIndex;  // nフレームの平均をとる
+  private Vector3 rotationPreset;
+  private float rotationSpeed = 1f;
+  private float rotationDeltaSpeed;
+
+  void Start()
+  {
+  	_transform = this.transform;
+  }
+
+  void Update ()
+  {
+    mousePosition = Input.mousePosition;
+    mousePosition.z = 10f;
+    screenPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    Vector3 diff = (screenPosition - _transform.position);
+    if(Input.GetMouseButton(0))
+    {
+      _transform.rotation = Quaternion.FromToRotation (Vector3.up, diff);
+    }
+    else if(Input.GetMouseButtonUp(0))
+    {
+      rotationDeltaSpeed = GetAvgDeltaSpeed(prevPosition);
+      rotationPreset = new Vector3(0,0,_transform.rotation.z);
+    }
+    else
+    {
+    	_transform.Rotate(new Vector3(0, 0, rotationDeltaSpeed) * Time.deltaTime *-1f, Space.World);
+    }
+    if(prevPositionIndex >= prevPosition.Length-1)
+    {
+    	prevPositionIndex=0;
+    }else{
+    	prevPositionIndex+=1;
+    }
+    prevPosition[prevPositionIndex] = screenPosition;
+  }
+
+  public float GetDiff(Vector2 p1, Vector2 p2)
+  {
+    float dx1 = 0f - p1.x;
+    float dy1 = 0f - p1.y;
+    float dx2 = 0f - p2.x;
+    float dy2 = 0f - p2.y;
+    float rad1 = Mathf.Atan2(dy1, dx1);
+    float rad2 = Mathf.Atan2(dy2, dx2);
+    float diff = rad1 * Mathf.Rad2Deg - rad2 * Mathf.Rad2Deg;
+    return diff;
+  }
+
+  public float GetAvgDeltaSpeed(Vector3[] pos)
+  {
+  	float sum = 0f;
+  	for(int i=0;i<pos.Length;i++)
+  	{
+  		sum += GetDiff(screenPosition, pos[i]);
+  		print(i.ToString()+" : "+GetDiff(screenPosition, pos[i]).ToString());
+  	}
+  	return sum/pos.Length;
+  }
+}
---
+++ Scripts/RotationCounter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotationCounter : MonoBehaviour {
+
+  public GameObject textObj;
+  private Text _text;
+
+  private Transform _transform;
+
+  private int forwardCount;
+  private int reverceCount;
+  private int rotationCount;
+  private int rotationPart;
+  private int prevRotationPart;
+
+  // Use this for initialization
+  void Start () {
+    _transform = this.transform;
+    _text = textObj.GetComponent<Text>();
+  }
+  
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    float angleZ = _transform.eulerAngles.z;
+    if(angleZ < 120f)
+    {
+      rotationPart = 1;
+    }
+    else if(angleZ < 240f)
+    {
+      rotationPart = 2;
+    }
+    else
+    {
+      rotationPart = 3;
+    }
+
+    if ((rotationPart == prevRotationPart + 1)|(rotationPart == prevRotationPart - 2))
+    {
+      if (rotationCount>0)
+      {
+        rotationCount=0;
+      }
+      else{
+        rotationCount-=1;
+      }
+    }
+    else if ((rotationPart == prevRotationPart - 1)|(rotationPart == prevRotationPart + 2))
+    {
+      if (rotationCount<0)
+      {
+        rotationCount=0;
+      }
+      else
+      {
+        rotationCount+=1;
+      }
+    }
+    if(rotationCount>=3)
+    {
+      rotationCount = 0;
+      forwardCount+=1;
+    }else if(rotationCount<=-3)
+    {
+      rotationCount = 0;
+      reverceCount+=1;
+    }
+
+    if(_text!=null)
+    {
+      _text.text = (forwardCount+reverceCount).ToString();
+    }
+
+    prevRotationPart = rotationPart;
+
+  }
+}
